bad253fe5f88abbee701c3b4f76e2500123c4e25
Valery <qaivlev@yandex.ru>
2023-03-28 10:25:20 +0300

    add Task

--- Chess_Problems.py
+++ Chess_Problems.py
@@ -12,7 +12,7 @@ else:
 # и "NO" (без кавычек) в противном случае.
 
 
-a, b, c, d  = int(input()), int(input()), int(input()), int(input())
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
 if abs(a - c) <= 1 and  abs(b - d) <= 1:
     print("YES")
 else:
