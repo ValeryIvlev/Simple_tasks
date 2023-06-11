7c9e8c59dbe56efb47119d80254f3fb4d833fece
Valery <qaivlev@yandex.ru>
2023-06-11 15:50:01 +0300

    add Task

--- leetcode.py
+++ leetcode.py
@@ -56,3 +56,21 @@ class Solution:
             return True
         else:
             return False
+
+# 66. Plus One
+
+#You are given a large integer represented as an integer array digits,
+# where each digits[i] is the ith digit of the integer.
+# The digits are ordered from most significant to least significant in left-to-right order.
+# The large integer does not contain any leading 0's.
+
+#Increment the large integer by one and return the resulting array of digits.
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = list(map(str, digits))
+        a = ''.join(digits)
+        a = str(int(a) + 1)
+        s = list(map(int, [i for i in a]))
+        return s
