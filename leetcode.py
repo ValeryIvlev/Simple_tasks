ef31775158ccdf5b14bc5dac6622786598c7e48e
Valery <qaivlev@yandex.ru>
2023-06-17 11:11:43 +0300

    add Task

--- leetcode.py
+++ leetcode.py
@@ -111,3 +111,16 @@ class Solution:
             return True
         else:
             return False
+
+# 1491. Average Salary Excluding the Minimum and Maximum Salary
+
+# You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
+#
+# Return the average salary of employees excluding the minimum and maximum salary.
+# Answers within 10-5 of the actual answer will be accepted.
+
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary.sort()
+        salary = salary[1:-1]
+        return sum(salary) / len(salary)
