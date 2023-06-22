e325be449d037a3f738a0204abade3aa0c7993d4
Valery <qaivlev@yandex.ru>
2023-06-22 10:28:07 +0300

    add Task

--- leetcode.py
+++ leetcode.py
@@ -124,3 +124,24 @@ class Solution:
         salary.sort()
         salary = salary[1:-1]
         return sum(salary) / len(salary)
+
+# 1859. Sorting the Sentence
+
+# A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+# Each word consists of lowercase and uppercase English letters.
+#
+# A sentence can be shuffled by appending the 1-indexed word position to each word
+# then rearranging the words in the sentence.
+#
+# For example, the sentence "This is a sentence" can be shuffled
+# as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+# Given a shuffled sentence s containing no more than 9 words,
+# reconstruct and return the original sentence.
+
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        s = s.split()
+        s.sort(key=lambda x: x[-1])
+        s = [i[0:len(i)-1] for i in s]
+
+        return ' '.join(s)
