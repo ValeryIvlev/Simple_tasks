9356dad0ebb61dbe3be6f486dbabf852eb763785
Valery <qaivlev@yandex.ru>
2023-07-02 12:01:49 +0300

    add Task

--- leetcode.py
+++ leetcode.py
@@ -195,3 +195,24 @@ class Solution:
                 two = j
                 break
         return [one+1, two+1]
+
+
+# 1221. Split a String in Balanced Strings
+
+# Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+#
+# Given a balanced string s, split it into some number of substrings such that:
+#
+# Each substring is balanced.
+# Return the maximum number of balanced strings you can obtain
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        a = []
+        count = 0
+        for i in s:
+            a.append(i)
+            if a.count('L') == a.count('R'):
+                count += 1
+                a.clear()
+        return count
