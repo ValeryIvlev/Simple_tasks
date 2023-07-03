1b1e536ae16981e961e4dfc2d6d665dfb5a63c4a
Valery <qaivlev@yandex.ru>
2023-07-03 12:56:34 +0300

    add Task

--- leetcode.py
+++ leetcode.py
@@ -216,3 +216,13 @@ class Solution:
                 count += 1
                 a.clear()
         return count
+
+
+# 557. Reverse Words in a String III
+
+# Given a string s, reverse the order of characters
+# in each word within a sentence while still preserving whitespace and initial word order.
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join([i[::-1] for i in s.split()])
