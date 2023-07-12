adc70d8e2130ea58fe0c81dc9a5930892fee8542
Valery <qaivlev@yandex.ru>
2023-07-12 13:05:40 +0300

    add Task

--- leetcode.py
+++ leetcode.py
@@ -226,3 +226,16 @@ class Solution:
 class Solution:
     def reverseWords(self, s: str) -> str:
         return ' '.join([i[::-1] for i in s.split()])
+
+
+# 692. Top K Frequent Words
+
+# Given an array of strings words and an integer k, return the k most frequent strings.
+# Return the answer sorted by the frequency from highest to lowest.
+# Sort the words with the same frequency by their lexicographical order.
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        words = [(words.count(i), i) for i in words]
+        a = sorted(list(set(words)), key=lambda x: (-x[0], x[1]),)[:k]
+        return [i[1] for i in a]
