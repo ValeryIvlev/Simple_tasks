01e01ec0237b4db6b286243354fbfed8b21b1f0c
Valery <qaivlev@yandex.ru>
2023-07-28 08:18:42 +0300

    add Task

--- codewars.py
+++ codewars.py
@@ -27,3 +27,27 @@ def move_zeros(lst):
 def domain_name(url):
     new_url = url.replace('http://', '').replace('https://', '').replace('www.', '')
     return new_url[:new_url.find('.')]
+
+# 6 kyu
+# Highest Scoring Word
+
+# Given a string of words, you need to find the highest scoring word.
+# Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+# For example, the score of abad is 8 (1 + 2 + 1 + 4).
+# You need to return the highest scoring word as a string.
+# If two words score the same, return the word that appears earliest in the original string.
+# All letters will be lowercase and all inputs will be valid.
+
+def high(x):
+    x = x.split()
+    maximum = 0
+    word = ''
+    for i in x:
+        minimum = 0
+        for j in i:
+            minimum += ord(j) - 96
+            if maximum < minimum:
+                maximum = minimum
+                word = i
+
+    return word
